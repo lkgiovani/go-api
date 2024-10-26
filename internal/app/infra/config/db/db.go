e1a056f251c87f94cd81e05320cf281f1f35ba94
giovani souza <giovani.souza@insidecode.io>
2024-10-26 01:13:05 -0300

    feat(db): add db

--- internal/app/infra/config/db/db.go
+++ internal/app/infra/config/db/db.go
@@ -17,7 +17,7 @@ import (
 // NewDBConfig establishes a new database connection based on the provided configuration.
 func NewDBConfig(config *configEnv.Config) (*sql.DB, error) {
 	// Create a DSN (Data Source Name) from the configuration.
-	dns := config.Mysql.Url
+	dns := config.Mysql.Url + "bdtest"
 	// Open a database connection using the DSN.
 	db, err := sql.Open("mysql", dns)
 	if err != nil {
@@ -46,83 +46,72 @@ func NewDBConfig(config *configEnv.Config) (*sql.DB, error) {
 	return db, nil
 }
 
-// InitDB initializes the database using the provided lifecycle and configuration.
-func InitDB(lc fx.Lifecycle, db *sql.DB, config *configEnv.Config) error {
-	// Define o ciclo de vida do banco de dados no Fx
+func InitDB(lc fx.Lifecycle, config *configEnv.Config) (*sql.DB, error) { // Passo 1: Conexão inicial ao MySQL sem especificar o banco// Passo 1: Conexão inicial ao MySQL sem especificar o banco
+	db, err := sql.Open("mysql", config.Mysql.Url)
+	if err != nil {
+		return nil, &projectError.Error{
+			Code:      projectError.EINTERNAL,
+			Message:   "failed to connect to MySQL server",
+			Path:      "config/db/InitDB.go",
+			PrevError: err,
+		}
+	}
+	defer db.Close() // Fecha esta conexão inicial ao final
+
+	// Passo 2: Criação do banco de dados, se necessário
+	databaseName := "bdtest"
+	err = createDatabase(db, databaseName)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Banco de dados", databaseName, "criado com sucesso ou já existia.")
+
+	// Passo 3: Conectar-se com o banco de dados `bdtest`
+	db, err = sql.Open("mysql", config.Mysql.Url+"bdtest")
+	if err != nil {
+		return nil, &projectError.Error{
+			Code:      projectError.EINTERNAL,
+			Message:   "failed to connect to database",
+			Path:      "config/db/InitDB.go",
+			PrevError: err,
+		}
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, &projectError.Error{
+			Code:      projectError.EINTERNAL,
+			Message:   "failed to ping database",
+			Path:      "config/db/InitDB.go",
+			PrevError: err,
+		}
+	}
+
+	// Passo 4: Executar o arquivo SQL de migração
+	sqlFilePath := "internal/app/infra/config/db/migrations/migrationInit.sql"
+	err = executeSQLFile(db, sqlFilePath)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Arquivo de migração SQL executado com sucesso.")
+
+	// Configuração do ciclo de vida com Fx
 	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error { // Adiciona o contexto como parâmetro
-
-			fmt.Println("Iniciando banco de dados...")
-
-			// Define o nome do banco de dados
-			databaseName := "BDTest"
-
-			// Cria o banco de dados, se não existir
-			err := createDatabase(db, databaseName)
-			if err != nil {
-				return &projectError.Error{
-					Code:      projectError.EINTERNAL,
-					Message:   "failed to create database",
-					Path:      "config/db/InitDB.go",
-					PrevError: err,
-				}
-			}
-
-			// Cria o DSN para o banco de dados
-			dbNameDSN := fmt.Sprintf(config.Mysql.Url + databaseName)
-			// Abre a conexão com o banco de dados usando o DSN
-			db, err = sql.Open("mysql", dbNameDSN)
-			if err != nil {
-				return &projectError.Error{
-					Code:      projectError.EINTERNAL,
-					Message:   "failed to connect to database",
-					Path:      "config/db/InitDB.go",
-					PrevError: err,
-				}
-			}
-
-			// Verifica a conexão com o banco de dados
-			err = db.Ping()
-			if err != nil {
-				return &projectError.Error{
-					Code:      projectError.EINTERNAL,
-					Message:   "failed to ping database",
-					Path:      "config/db/InitDB.go",
-					PrevError: err,
-				}
-			}
-
-			// Executa o arquivo SQL para inicializar o banco de dados
-			sqlFilePath := "internal/app/infra/config/db/migrations/migrationInit.sql"
-			err = executeSQLFile(db, sqlFilePath)
-			if err != nil {
-				return &projectError.Error{
-					Code:      projectError.EINTERNAL,
-					Message:   "failed to execute SQL file",
-					Path:      "config/db/InitDB.go",
-					PrevError: err,
-				}
-			}
-			return nil
-		},
-		OnStop: func(ctx context.Context) error { // Adiciona o contexto como parâmetro
+		OnStop: func(ctx context.Context) error {
 			fmt.Println("Fechando conexão com o banco de dados...")
-			// Fecha a conexão com o banco de dados
 			return db.Close()
 		},
 	})
 
-	return nil
+	fmt.Println("Conexão com o banco de dados configurada e pronta.")
+	return db, nil
 }
 
 // createDatabase creates the database if it doesn't exist.
 func createDatabase(d *sql.DB, name string) error {
-	// Create a query to create the database.
 	query := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", name)
-	// Execute the query using the database connection.
-	_, err := d.Exec(query) // Use the db passed as a parameter
+	_, err := d.Exec(query)
 	if err != nil {
-		// Return an error if the query execution fails.
 		return &projectError.Error{
 			Code:      projectError.ENOTFOUND,
 			Message:   "Database not found. " + err.Error(),
@@ -130,23 +119,17 @@ func createDatabase(d *sql.DB, name string) error {
 			PrevError: err,
 		}
 	}
-
 	return nil
 }
 
 // executeSQLFile reads and executes the SQL file at the given file path.
-// It returns an error if the file cannot be read or the SQL execution fails.
 func executeSQLFile(db *sql.DB, filePath string) error {
-
-	// Obtenha o diretório de trabalho atual
 	workingDir, err := os.Getwd()
 	if err != nil {
 		return err
 	}
 
-	// Construa o caminho completo
 	fullPath := filepath.Join(workingDir, filePath)
-
 	sqlBytes, err := ioutil.ReadFile(fullPath)
 	if err != nil {
 		return &projectError.Error{
