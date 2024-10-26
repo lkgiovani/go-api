4a757330c6289e519109e53731ea10c808bef781
giovani souza <giovani.souza@insidecode.io>
2024-10-26 20:09:53 -0300

    feat(user_rep): add func GetAllUser

--- internal/app/repository/user_repo/user_repo.go
+++ internal/app/repository/user_repo/user_repo.go
@@ -6,6 +6,7 @@ import (
 )
 
 type UserRepository interface {
+	GetAllUser(ctx context.Context) []user_model.User
 	GetUserById(ctx context.Context, id string) user_model.User
 	SetUser(ctx context.Context, user user_model.User) error
 }
--- internal/app/repository/user_repo/user_repo_impl.go
+++ internal/app/repository/user_repo/user_repo_impl.go
@@ -3,7 +3,6 @@ package user_repo
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"go-api/internal/app/api/model/user_model"
 	"go-api/pkg/projectError"
 )
@@ -18,16 +17,44 @@ func NewUserRepository(db *sql.DB) *UserRepositoryImpl {
 	}
 }
 
-func (r *UserRepositoryImpl) GetUserById(ctx context.Context, id string) (user_model.User, error) {
-	user := user_model.User{}
+func (r *UserRepositoryImpl) GetAllUser(ctx context.Context) ([]user_model.User, error) {
+	users := []user_model.User{}
 
-	query := "SELECT id, name, email FROM users WHERE id = ?"
-	err := r.db.QueryRowContext(ctx, query, id).Scan(&user.Id, &user.Name, &user.Email)
+	query := "SELECT id, name, email FROM users"
+	rows, err := r.db.QueryContext(ctx, query)
 	if err != nil {
-		return user_model.User{}, &projectError.Error{Code: projectError.EINTERNAL, Message: err.Error()}
+		return nil, &projectError.Error{
+			Code:      projectError.EINTERNAL,
+			Message:   "failed query all user",
+			PrevError: err,
+			Path:      "repository/user_repo/user_repo_impl.go",
+		}
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user user_model.User
+		if err := rows.Scan(&user.Id, &user.Name, &user.Email); err != nil {
+			return nil, &projectError.Error{
+				Code:      projectError.EINTERNAL,
+				Message:   "failed to get all user",
+				PrevError: err,
+				Path:      "repository/user_repo/user_repo_impl.go",
+			}
+		}
+		users = append(users, user)
 	}
 
-	return user, nil
+	if err := rows.Err(); err != nil {
+		return nil, &projectError.Error{
+			Code:      projectError.EINTERNAL,
+			Message:   "failed to get all user",
+			PrevError: err,
+			Path:      "repository/user_repo/user_repo_impl.go",
+		}
+	}
+
+	return users, nil
 }
 
 func (r *UserRepositoryImpl) SetUser(ctx context.Context, user user_model.User) error {
@@ -37,7 +64,34 @@ func (r *UserRepositoryImpl) SetUser(ctx context.Context, user user_model.User) 
 		return &projectError.Error{Code: projectError.EINTERNAL, Message: err.Error()}
 	}
 
-	fmt.Println("salve1")
-
 	return nil
+}
+
+func (r *UserRepositoryImpl) GetUserById(id string) (user_model.User, error) {
+	var user user_model.User
+
+	query := `SELECT id, name, email FROM users WHERE id = ?`
+	rows := r.db.QueryRowContext(context.Background(), query, id)
+
+	if err := rows.Err(); err != nil {
+		return user, &projectError.Error{
+			Code:      projectError.EINTERNAL,
+			Message:   "failed to get all user",
+			PrevError: err,
+			Path:      "repository/user_repo/user_repo_impl.go",
+		}
+	}
+
+	err := rows.Scan(&user.Id, &user.Name, &user.Email)
+	if err != nil {
+		return user, &projectError.Error{
+			Code:      projectError.EINTERNAL,
+			Message:   "failed to get all user",
+			PrevError: err,
+			Path:      "repository/user_repo/user_repo_impl.go",
+		}
+	}
+
+	return user, nil
+
 }
