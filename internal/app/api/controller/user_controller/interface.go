1fff528b9e4ff2be2a826e63c289a3fdcf54b8d1
giovani souza <giovani.souza@insidecode.io>
2024-10-26 20:10:13 -0300

    feat(user_controller): add func GetAllUser and GetUserById

--- internal/app/api/controller/user_controller/interface.go
+++ internal/app/api/controller/user_controller/interface.go
@@ -1,7 +1,11 @@
 package user_controller
 
-import "go-api/internal/app/api/model/user_model"
+import (
+	"go-api/internal/app/api/model/user_model"
+)
 
 type UserControllerInterface interface {
 	SetUser(user_model.User) error
+	GetAllUser() ([]user_model.User, error)
+	GetUserById(id string) (user_model.User, error)
 }
